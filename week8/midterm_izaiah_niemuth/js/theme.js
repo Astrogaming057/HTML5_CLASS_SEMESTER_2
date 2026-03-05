a5723920a819684232aecc79662827fdbdf555f5
Niemuth, Izaiah <800083397@fvtc.edu>
2026-03-05 16:20:13 -0600

    week 8 finished and week 7 fixed/murged

--- week8/midterm_izaiah_niemuth/js/theme.js
+++ week8/midterm_izaiah_niemuth/js/theme.js
@@ -1,6 +1,5 @@
 const c='classList';
 const a='darkmode';
-const n='darkMode';
 const p=document;
 const l=localStorage;
 const _t=()=>{
@@ -10,21 +9,21 @@ const _t=()=>{
     if(d){
         b[c].remove(a);
         w[c].remove(a);
-        l.setItem(n,'0');
+        l.setItem(a,'0');
     }else{
         b[c].add(a);
         w[c].add(a);
-        l.setItem(n,'1');
+        l.setItem(a,'1');
     }
 };
 p.addEventListener('DOMContentLoaded',()=>{
-    const s=l.getItem(n);
+    const s=l.getItem(a);
     const b=p.body;
     const w=p.getElementById('wrapper');
     if(s!=='0'){
         b[c].add(a);
         w[c].add(a);
-        l.setItem(n,'1');
+        l.setItem(a,'1');
     }else{
         b[c].remove(a);
         w[c].remove(a);
